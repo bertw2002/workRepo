d1de0875599d86e9e9d0d7da0fda7b8f462b7696
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-09-10 22:20:32 -0400

    finished code

--- randTeam.py
+++ randTeam.py
@@ -1,8 +1,17 @@
 import random
 
 def randPerson(arr):
-    weirdName = random.randint(0, 3)
-    print weirdName
+    weirdNamesint = random.randint(0, 2)
+    arr1 = list()
+    for wdName in arr:
+        arr1.append(wdName)
+    #print(arr1)
+    weirdName = arr1[weirdNamesint]
+    #print(weirdName)
+    arrNames = arr[weirdName]
+    #print(arrNames)
+    nameLength = len(arrNames)
+    print(arrNames[random.randint(0, nameLength)])
 
 KREWES = {
     'orpheus': ['Emily', 'Kevin', 'Vishwaa', 'Eric', 'ray', 'Jesse', 'Tiffany',
