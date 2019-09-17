5f3d3d89682c555aff1131a0523c0561aa9a4135
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-09-16 22:26:03 -0400

    finished first func

--- 03_csv/ToiletPaper_wanA-tanM.py
+++ 03_csv/ToiletPaper_wanA-tanM.py
@@ -1,1 +1,24 @@
+#Albert Wan
+#SoftDev1 pd9
+#K06 -- Divine your Destiny!
+#2019-09-17
 
+
+import random
+import csv
+
+def createDict(csvFile):
+    newDict = {}
+    with open(filename) as oldcsv:
+        counter = 0 # counter to skip first row, because it's just the heading
+        csvRead = csv.reader(oldcsv, delimiter = ',')
+        for row in csvRead:
+            if counter > 0:
+                newDict[row[0]] = float(row[1]) #i dont know if i need float, but there is a decimal so probably....
+            counter += 1
+    del newDict['Total']
+    return newDict
+
+
+def createArray(newDict):
+    
