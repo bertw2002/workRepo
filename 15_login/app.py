2dece1f2de7da2460294e53f20f72388882d2b63
Albert Wan <awan00@stuy.edu>
2019-10-02 14:35:45 -0400

    app.py and page html

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template, request
+import random
+app = Flask(__name__)
 
+@app.route("/")
+def occupy():
+    print("hello")
+    return render_template("page.html")
+
+@app.route("/auth")
+def route():
+    return render_template
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
