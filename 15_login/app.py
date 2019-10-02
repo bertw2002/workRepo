35060539ade8292da5cd1186e7d13813d9b4c7dc
Albert Wan <awan00@stuy.edu>
2019-10-02 14:54:45 -0400

    Establish site map, and add in placeholders for what to do during logins

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import random
 app = Flask(__name__)
 
 @app.route("/")
 def occupy():
     print("hello")
-    return render_template("page.html")
+    return render_template(
+        "page.html",
+        username = request.args['username'],
+        password = request.args['password']
+        )
+
+logins = {'bertw2002':'oreo'}
 
 @app.route("/auth")
 def route():
-    return render_template
+    if logins[username] == password:
+        return redirect("https://xkcd.com") # replace with login page
+    if not username in logins.keys():
+        return redirect('blah') # replace with create account page
 
 if __name__ == "__main__":
     app.debug = True
