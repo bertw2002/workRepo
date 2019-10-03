4a753466ce96ef00ed6db77c235e38d6b74e0eb3
Albert Wan <awan00@stuy.edu>
2019-10-03 14:37:43 -0400

    added updated login

--- 15_login/app.py
+++ 15_login/app.py
@@ -1,24 +1,56 @@
-from flask import Flask, render_template, request, redirect, url_for
-import random
+# Winston Peng & Albert Wan -- Team Dental Hygeine
+# SoftDev1 pd9
+# K15 -- Do I Know You? -- Using Cookies, Login Page
+# 10-2-2019
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+
+user = {}
 
 @app.route("/")
 def occupy():
     print("hello")
+#    username = request.args['username']
+#    password = request.args['password']
+    print(request.cookies)
+#    if request.cookies.get['login']:  # replace with actual conditional
+#        redirect('/account')
     return render_template(
         "page.html",
-        username = request.args['username'],
-        password = request.args['password']
-        )
+    )
 
-logins = {'bertw2002':'oreo'}
+
+logins = {'bertw2002': 'oreo'}
+
 
 @app.route("/auth")
 def route():
-    if logins[username] == password:
-        return redirect("https://xkcd.com") # replace with login page
-    if not username in logins.keys():
-        return redirect('blah') # replace with create account page
+    """this is a temporary page that checks if login was correct"""
+    username = request.args['username']
+    password = request.args['password']
+    print(request.cookies)
+    print('decoy')
+    if username not in logins.keys():
+        return redirect('/newuser')
+    if username == password:
+        return redirect('/login')
+    return redirect('/account')
+
+@app.route('/account')
+def login():
+    """prints welcome message in account page"""
+    #user = request.args['username']
+    return render_template(
+        'account.html'
+        #user=username
+    )
+
+@app.route('/newuser')
+def newuser():
+    """This is where people can make a new account"""
+    return render_template(
+        'newuser.html'
+    )
 
 if __name__ == "__main__":
     app.debug = True
