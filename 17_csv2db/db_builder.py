ae2d86a2e996106583ce081b007845281efda698
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-10-09 23:42:06 -0400

    switched db

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -7,7 +7,7 @@ import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="db2.db"
+DB_FILE="discobandit.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
