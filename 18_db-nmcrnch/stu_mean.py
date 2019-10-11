d88690ab927706d4c0eaf42461bdcf256a381c50
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-10-10 20:10:04 -0400

    some code

--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -1,1 +1,12 @@
+#Albert Wan, Kenneth Chin
+#SoftDev1 pd9
+#K18 -- Average
+#2019-10-11
 
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitate CSV I/O
+
+DB_FILE="school.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
