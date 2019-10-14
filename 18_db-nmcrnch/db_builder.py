1eb47f5fe4e61adc04287d140d7a2c38c45f2656
Albert Wan <albert@Alberts-MacBook-Air.local>
2019-10-14 00:02:40 -0400

    added code

--- 18_db-nmcrnch/db_builder.py
+++ 18_db-nmcrnch/db_builder.py
@@ -1,46 +1,33 @@
-  
-#Albert Wan
-#SoftDev1 pd9
-#K17 - No Trouble
-#2019-10-10
+# Jun Tao Lei & Kenneth Chin
+# SoftDev1 pd9
+# K#17 - No Trouble
+# 2019-10-07
 
-import sqlite3   #enable control of an sqlite database
-import csv       #facilitate CSV I/O
+from sqlite3 import connect, Row
+from utl.csvrw import insertAll, printTable
 
+# Setup the database
+DB_FILE = "data.db"
 
-DB_FILE="discobandit.db"
+db = connect(DB_FILE)
+db.row_factory = Row
+c = db.cursor()
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops
+# Add the csv and create a table to store the csv if it does not exist
+insertAll("data/courses.csv", "courses", db)
+insertAll("data/students.csv", "students", db)
 
-#==========================================================
+# Print the database
+print("courses table")
+printTable("courses", c)
+print("\nstudents table")
+printTable("students", c)
+q = """SELECT name, students.id, mark
+FROM students, courses
+WHERE students.id = courses.id;"""
 
-# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
-
-
-command = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);"
-#create table in mySql
-c.execute(command)
-with open('courses.csv') as courseFile:
-    courseReader = csv.DictReader(courseFile)
-    #reads the file
-
-    for row in courseReader:
-#        exec = "INSERT INTO courses VALUES ('{}', {}, {});".format(x['code'], x['mark'], x['id'])
-        command = "INSERT INTO courses VALUES(\"" + row['code'] + "\"" + ", " + row['mark'] + ", " + row['id'] + ");"
-        #now, you populate values into the table
-        c.execute(command)
-command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"
-#create table in mySql
-c.execute(command)
-with open('students.csv') as studentFile:
-    studentReader = csv.DictReader(studentFile)
-    #reads the file
-    for row in studentReader:
-        exec = "INSERT INTO students VALUES(\"" + row['name'] + "\"" + ", " + row['age'] + ", " + row['id'] + ");"
-        #now, you insert each value into the table (or populate)
-        c.execute(exec)
-
-
-db.commit() #save changes
-db.close()  #close database
+foo = c.execute(q)
+print(foo)
+# Save and exit the database
+db.commit()
+db.close()
--- 18_db-nmcrnch/stu_mean.py
+++ 18_db-nmcrnch/stu_mean.py
@@ -5,8 +5,33 @@
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
-
-DB_FILE="school.db"
+from utl.csvrw import insertAll, printTable
+DB_FILE="data.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
+
+
+#computes student's average
+average = """SELECT name, students.id, SUM(mark) / COUNT(mark)
+    FROM students, courses
+    WHERE students.id = courses.id
+    GROUP BY courses.id;"""
+
+newTable = "CREATE TABLE stu_avg(id INTEGER, average INTEGER)"
+
+# creates table stu_avg
+# c.execute(newTable)
+c.execute(average)
+
+averages = c.fetchall()
+print (averages)
+
+for x in averages:
+    print (x[0], ":",  x[1], ":", x[2])
+    insert = "INSERT INTO stu_avg VALUES(" + str(x[1]) + "," +  str(x[2]) + ")" + ";"
+    c.execute(insert)
+
+
+db.commit() #save changes
+db.close()  #close database
