23b053770091595323443ce962b45e210a48f193
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-12-10 22:32:10 -0500

    finished

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,9 +1,22 @@
-function fibonacci(n) {
+var fibonacci = function(n) {
   if (num == 0) return 0;
   if (num == 1) return 1;
   return fibonacci(num - 1) + fibonacci(num - 2);
-}
+};
 
-function gcd(a, b) {
-  
-}
+var gcd = function(a, b) {
+  if (a % b == 0){
+    return b;
+  }
+  if (b > a){
+    return gcd(b, a);
+  }
+  return gcd(a - b, b);
+};
+
+var randlist = ["topher", "Albert", "Kazi", "Nahi"]
+var randomStudent = function(){
+  x = Math.random();
+  listlength = randlist.length;
+  return randlist[Math.floor(listlength * x)];
+};
