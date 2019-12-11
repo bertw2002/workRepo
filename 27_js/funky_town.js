432bb07a929514d768fb4a3460903041d2f7b39b
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-12-10 22:45:57 -0500

    fixed error

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -1,7 +1,7 @@
 var fibonacci = function(n) {
-  if (num == 0) return 0;
-  if (num == 1) return 1;
-  return fibonacci(num - 1) + fibonacci(num - 2);
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
 };
 
 var gcd = function(a, b) {
