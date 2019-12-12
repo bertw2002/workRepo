37e70397e3322878d69e3498520f7dd9a4099cc9
Albert Wan <albert@Alberts-Air.fios-router.home>
2019-12-12 00:28:03 -0500

    m

--- 28_jsdom/funky_town.js
+++ 28_jsdom/funky_town.js
@@ -1,22 +1,54 @@
+// Nahi Khan & Albert Wan
+// SoftDev1 pd9
+// K28 - Sequential Progression II: Electric Boogaloo...
+// 2019 - 12 - 11
+
+var foo = function() {
+    console.log("foo testing");
+};
+
+var factorial = function(n) {
+    if (n <= 1) {
+	return 1;
+    }
+    return n * factorial(n-1);
+};
+
 var fibonacci = function(n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  return fibonacci(n - 1) + fibonacci(n - 2);
+    if (n == 0) {
+	return 0;
+    };
+    if (n == 1) {
+	return 1;
+    };
+    return fibonacci(n-1) + fibonacci(n-2);
 };
 
 var gcd = function(a, b) {
-  if (a % b == 0){
-    return b;
-  }
-  if (b > a){
-    return gcd(b, a);
-  }
-  return gcd(a - b, b);
+    if (a == 0 || b == 0) {
+	return "a or b cannot be 0";
+    };
+    if (b > a) {
+	return gcd(b, a);
+    };
+    if (a % b == 0) {
+	return b;
+    };
+    gcd(b, a % b);
 };
 
-var randlist = ["topher", "Albert", "Kazi", "Nahi"]
-var randomStudent = function(){
-  x = Math.random();
-  listlength = randlist.length;
-  return randlist[Math.floor(listlength * x)];
+var students = ["kazi", "albert", "nahi", "john", "jane"];
+
+var randomStudent = function(students) {
+    var randIndex = Math.floor(Math.random() * students.length);
+    return students[randIndex];
 };
+
+var printFib = function() {
+    var ans = fibonacci(8)
+    console.log(ans);
+    return ans;
+};
+
+var fibbtn = document.getElementById("fibbtn");
+fibbtn.addEventListener('click', printFib);
