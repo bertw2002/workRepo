b53dc869d5b1bffd6abb0d8b0b50020ea6ca1338
Albert Wan <awan00@stuy.edu>
2019-12-13 14:29:13 -0500

    m

---
+++ 29_jsdom/dommy.js
@@ -0,0 +1,36 @@
+var changeHeading = function(e) {
+  var h = document.getElementById("h");
+  h.innerHTML = lis;
+};
+
+var removeItem = function(e) {
+
+}
+
+var lis = document.getElementsbyTagzname("li");
+
+for (var i=0; o<lis.length;i++){
+  lis[i].addEventListener('mouseover', )
+  lis[i].addEventListener('mouseout', )
+
+  list[i].addEventListener('click', )
+}
+
+var addItem = function(e) {
+  var list =
+  var item = document.createElement("li");
+  //button.addEventListener('click', function(e) {console.log(e)}) = "WORD";
+  ???
+  ...
+  ???
+  list.???(item);
+}
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem);
+
+var fib = function(n) {
+  if ( n < 2 ){
+    return 1;
+  }
+}
