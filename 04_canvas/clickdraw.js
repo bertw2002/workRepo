55a64fd628dec55f71fe90151329f01bd2c170a2
Albert Wan <albert@Alberts-MacBook-Air.local>
2020-02-05 14:50:38 -0500

    code

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,1 +1,12 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+
+funcion clearrect() {
+
+  const context = canvas.getContext('2d');
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
