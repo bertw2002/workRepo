484ae373df13b872cabc6fdefcc662e0bf990a0f
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-02-05 22:54:55 -0500

    m

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -2,10 +2,10 @@
 //SoftDev2 pd9
 //K04 -- I See a Red Door...
 //2020-02-06
-var canvas = document.getElementById('slate');
-var cleary = document.getElementById("clear");
+var canvas = document.getElementById("slate");
+var cleary = document.getElementById("clearrect");
 var toggley = document.getElementById("toggle");
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext("2d");
 var which = true
 
 
@@ -29,6 +29,6 @@ var clearrect = function(e){
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-cleary.addEventListener('click', clear);
+cleary.addEventListener('click', clearrect);
 toggley.addEventListener('click', whichdraw);
 canvas.addEventListener('click', addshape);
