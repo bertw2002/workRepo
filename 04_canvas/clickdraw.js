57805041116f2722fce60323927d903dd0cad04b
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-02-05 22:52:29 -0500

    m

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,12 +1,34 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+//Albert Wan
+//SoftDev2 pd9
+//K04 -- I See a Red Door...
+//2020-02-06
+var canvas = document.getElementById('slate');
+var cleary = document.getElementById("clear");
+var toggley = document.getElementById("toggle");
+var ctx = canvas.getContext('2d');
+var which = true
 
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
 
-funcion clearrect() {
-
-  const context = canvas.getContext('2d');
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
+var whichdraw = function(e){
+  if (which){
+    which = false
+  }
 }
+var addshape = function(e){
+  if (which){
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(e.layerX, e.layerY, 100, 50);
+  }else{
+    console.log("ellipse");
+    ctx.fillStyle = 'green';
+    ctx.ellipse(e.layerX, e.layerY, 30,30, 0,0, Math.PI*2);
+    ctx.fill();
+  }
+}
+var clearrect = function(e){
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+cleary.addEventListener('click', clear);
+toggley.addEventListener('click', whichdraw);
+canvas.addEventListener('click', addshape);
