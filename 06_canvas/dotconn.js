3b20382a307f7f40ed2a17e55a186ed430de1284
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-02-11 22:24:12 -0500

    m

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,3 +1,20 @@
+var cleary = document.getElementById("clear");
+var ctx = canvas.getContext("2d");
+var canvas = document.getElementById("slate");
+
 var clear = function(e){
+  ctx.beginPath();
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+var makedot = function(e){
+  var xcor = e.offsetX;
+  var ycor = e.offsetY;
+  ctx.fillStyle = 'blue';
+  ctx.ellipse(xcor, ycor, 5, 5, 0, 0, Math.PI*2);
+  ctx.fill();
+  ctx.moveTo(xcor, ycor);
+  ctx.stroke();
+}
+cleary.addEventListener('click', clear);
+canvas.addEventListener('click', makedot);
