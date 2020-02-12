e7bab1e88d84dd0c12f50ab66b999dc9fc138285
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-02-11 22:28:59 -0500

    m

--- 06_canvas/dotconn.js
+++ 06_canvas/dotconn.js
@@ -1,6 +1,13 @@
+/*
+Albert Wan
+SoftDev pd9
+K06: Dot Dot Dot
+2020/2/11
+*/
 var cleary = document.getElementById("clear");
-var ctx = canvas.getContext("2d");
 var canvas = document.getElementById("slate");
+var ctx = canvas.getContext("2d");
+
 
 var clear = function(e){
   ctx.beginPath();
@@ -11,7 +18,7 @@ var makedot = function(e){
   var xcor = e.offsetX;
   var ycor = e.offsetY;
   ctx.fillStyle = 'blue';
-  ctx.ellipse(xcor, ycor, 5, 5, 0, 0, Math.PI*2);
+  ctx.ellipse(xcor, ycor, 3, 3, 0, 0, Math.PI*2);
   ctx.fill();
   ctx.moveTo(xcor, ycor);
   ctx.stroke();
