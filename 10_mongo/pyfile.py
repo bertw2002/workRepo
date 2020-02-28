ee0cbe557a25e8e6f4d044eecd08a310df4b1151
Albert Wan <awan00@stuy.edu>
2020-02-28 14:55:44 -0500

    M

--- 10_mongo/pyfile.py
+++ 10_mongo/pyfile.py
@@ -1,1 +1,18 @@
+#Albert Wan, Calvin Chu, Team Short Circuit
+#SoftDev pd9
+#K10: Import/Export Bank
+#2020-02-28
 
+from pymongo import MongoClient
+from bson.json_util import loads
+import pprint
+client = MongoClient('localhost', 27017)
+db = client.test
+collection = db.restaurants
+if (not "restaurants" in db.list_collection_names()):
+    f = open("data.json","r")
+    response = f.read()
+    data = loads(response)
+    f.close()
+    for post in data:
+        collection.insert_one(post)
