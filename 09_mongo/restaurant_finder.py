249fee2d316816fae54f66e2a6f4d45d0a271673
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-02-27 23:08:21 -0500

    M

--- 09_mongo/restaurant_finder.py
+++ 09_mongo/restaurant_finder.py
@@ -7,13 +7,33 @@ from pymongo import MongoClient
 from bson.json_util import loads
 client = MongoClient('localhost', 27017)
 
-db1 = client.someDB
-db = db1.restaurants
+db1 = client.restaurants
+db1.databse.drop()
+db = db1.databse
+
+with open("primer-dataset.json", "r") as file:
+  data = file.readlines()
+  for x in data:
+    db.insert_one(loads(data))
 
 def get_borough(bor):
+    val = db.find({ "borough": bor})
+    for x in val:
+        print(x["name"])
+#get_borough("Brooklyn")
+
 
 def get_zip(zip):
+    val = db.find({"address.zipcode": zip})
+    for x in val:
+        print(x["name"])
 
 def get_zipgrade(zip, grade):
+    val = db.find({ "address.zipcode": zip, "grades.grade": grade })
+    for x in val:
+        print(x["name"])
 
 def get_zipthresh(zip, thresh):
+    val = db.find({"address.zipcode": zip, "grades.score": { "$lt": thresh }})
+    for x in val:
+        print(x["name"])
