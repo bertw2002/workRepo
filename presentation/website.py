5870f227fc7cf1f87243563d1d331da70edd7d4f
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-03-29 14:08:16 -0400

    M

--- presentation/website.py
+++ presentation/website.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def root():
     return render_template("page0.html")
 
+@app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
