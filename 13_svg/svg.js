0c5f313ac8a26899a7d6ffd176b1f63b6b63d215
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-03-31 15:02:09 -0400

    finally works

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -4,31 +4,39 @@
 //2020-03-31
 var clear = document.getElementById("clear");
 var canvas = document.getElementById("canvas");
-
+var candraw = true;
 var draw = function() {
-  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  c.setAttribute("cy", event.offsetY);
-  c.setAttribute("cx", event.offsetX);
-  c.setAttribute("r", 15);
-  c.setAttribute("fill", "black");
-  c.addEventListener("click", draw1);
-  canvas.appendChild(c);
+  if (candraw){
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cy", event.offsetY);
+    c.setAttribute("cx", event.offsetX);
+    c.setAttribute("r", 15);
+    c.setAttribute("fill", "black");
+    c.addEventListener("click", draw1);
+    canvas.appendChild(c);
+  }
+  candraw = true;
 
 }
-var draw1 = function() {
+
+var draw1 = function(e) {
   this.setAttribute("fill", "orange");
   this.addEventListener("click", erase);
+  e.stopPropagation();
 }
+var erase = function(e) {
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    randomY = Math.floor(Math.random()*550);
+    randomX = Math.floor(Math.random()*550);
+    c.setAttribute("fill", "black");
+    c.setAttribute("cy", randomY);
+    c.setAttribute("cx", randomX);
+    c.setAttribute("r", 15);
+    canvas.removeChild(this);
+    c.addEventListener("click",draw1);
+    canvas.appendChild(c);
+    candraw = false;
 
-var erase = function() {
-  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-
-  randomY = Math.floor(Math.random()*550);
-  randomX = Math.floor(Math.random()*550);
-  c.setAttribute("cy", randomY);
-  c.setAttribute("cx", randomX);
-  c.setAttribute("r", 15);
-  c.setAttribute("fill", "black");
 }
 
 var clearC = function() {
