6db35d579ff856aac75ed8b5dd0917c830a65a86
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-03-31 16:21:41 -0400

    moving but not working

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -7,7 +7,7 @@ var yIncrem = 1;
 var clear = document.getElementById("clear");
 var canvas = document.getElementById("canvas");
 var xtra = document.getElementById("xtra");
-var mo
+var movement = document.getElementById("move");
 var draw = function() {
 
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -15,39 +15,38 @@ var draw = function() {
     c.setAttribute("cx", event.offsetX);
     c.setAttribute("r", 15);
     c.setAttribute("fill", "black");
-    c.addEventListener("click", draw1);
     canvas.appendChild(c);
 
 
 }
 
 var circleMove = function(e) {
+  window.requestAnimationFrame(circleMove);
   var allCircles = canvas.children;
   var length = allCircles.length;
   for (var x = 0; x < length; x++){
-    if (Math.floor(Math.random() * 2) = 0){
+    if (Math.floor(Math.random() * 2) == 0){
       xIncrem = 1;
     }else{
       xIncrem = -1;
     }
-    if (Math.floor(Math.random() * 2) = 0){
+    if (Math.floor(Math.random() * 2) == 0){
       yIncrem = 1;
     }else{
       yIncrem = -1;
     }
-    var xCor =  allCircles[x].getAttribute("cx") + xIncrem;
-    var yCor =  allCircles[x].getAttribute("yx") + yIncrem;
-    allCircles[x].setAttribute()
+    var xCor =  parseInt(allCircles[x].getAttribute("cx")) + xIncrem;
+    var yCor =  parseInt(allCircles[x].getAttribute("cy")) + yIncrem;
+    if (yCor <= 10 || yCor >= 570){
+      yIncrem = yIncrem * -1;
+    }
+    if (xCor <= 10 || xCor >= 570){
+      xIncrem = xIncrem * -1;
+    }
+    allCircles[x].setAttribute("cy", yCor);
+    allCircles[x].setAttribute("cx", xCor);
   }
 
-  if (yCor <= 10 || yCor >= 570){
-    yIncrem = yIncrem * -1;
-  }
-  if (xCor <= 10 || xCor >= 570){
-    xIncrem = xIncrem * -1;
-  }
-  circles[i].setAttribute("cy", y);
-  circles[i].setAttribute("cx", x);
 
 }
 
@@ -57,5 +56,5 @@ var clearC = function() {
   }
 }
 clear.addEventListener("click", clearC);
-
+movement.addEventListener("click", circleMove);
 canvas.addEventListener("click",draw);
