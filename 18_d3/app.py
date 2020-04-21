bed7c2a6195242070ce592734c0864bf67f4d10e
Albert Wan <albert@Alberts-Air.fios-router.home>
2020-04-21 15:08:11 -0400

    M

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -3,12 +3,26 @@
 # K18: Come Up For Air
 # 2020-04-20
 
-from flask import Flask, render_template
-import read_csv
+from flask import Flask, render_template, jsonify
+import csv
 app = Flask(__name__)
 @app.route("/")
-def home():
-    return render_template("index.html", data = read_csv.read_csv());
+def root():
+    return render_template("index.html");
+@app.route("/data")
+def data():
+    data = []
+    with open("static/tomato.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for entry in reader:
+
+            ["Year", "Score", "Title"]
+            data.append({"Year": int(entry[0]), "Score": int(entry[1]), "Title": entry[2]})
+
+    data = jsonify(data)
+    return data
+
 
 if __name__ == "__main__":
     app.debug = True
--- 18_d3/static/d3.js
+++ 18_d3/static/d3.js
@@ -1,1 +1,19 @@
+var renderB = document.getElementById("render");
+var transitionB = document.getElementById("transition");
+var index = 0;
+const category= ["Year", "Score", "Title"];
 
+
+
+var render function() {
+  index = 0;
+  transitionB.addEventListener("click", transition);
+
+}
+var transition function() {
+
+}
+
+
+
+renderB.addEventListener("click", render);
